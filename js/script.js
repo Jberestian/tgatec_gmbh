c4ba95f00dda4bf97e546fcedf12c8670bb21b65
Yevhen <you@example.com>
2020-05-03 14:18:08 +0200

    change project structure and create burger

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// "use strict";
+
+jQuery(document).ready(function() {
+
+  /*
+      Fullscreen Background
+  */
+$.backstretch(src="https://lh3.googleusercontent.com/proxy/AsnpkbxqQEjzdyBaNt6vFc7azojjyhGz74koAu1yP0oVzIGMa6RRsR_3gpR3JRn2YsEtvHnWl0xy4ejbJOQpBD0kPP3y_fqqFXntkz9rYsdJ3pfxSHzvvQ");
+
+/*
+    Open / Close Menu 
+*/
+$('.hamburger-button button').on('click', function(){
+  $('.top-menu').toggleClass('active');
+  $(this).toggleClass('menu-closed');
+});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//carousel function start
+// document.addEventListener('DOMContentLoaded', function() {
+    // let elems = document.querySelectorAll('.carousel');
+    // let instances = M.Carousel.init(elems);
+  // });
+  //carousel function end
+
+
