c93485b51825526f967083faac42030b41f57cee
Yevhen <you@example.com>
2020-05-27 18:28:25 +0200

    create footer

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function() {
   /*
       Fullscreen Background
   */
-$.backstretch(src="./img/jahnstr1.jpg");
+$.backstretch(src="./img/jahnstr/jahnstr_main.jpg");
 
 /*
     Open / Close Menu 
@@ -18,31 +18,3 @@ $('.hamburger-button button').on('click', function(){
 });
 
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
