0614e45378bcfbe1c7dd0d320cc1cafa9019c536
Yevhen <you@example.com>
2020-06-07 18:14:20 +0200

    change some stile for slider

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ jQuery(document).ready(function() {
 /*
     Open / Close Menu 
 */
+
 $('.hamburger-button button').on('click', function(){
   $('.top-menu').toggleClass('active');
   $(this).toggleClass('menu-closed');
